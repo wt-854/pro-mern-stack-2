f67d8d5b31c3a36dfdb4124f04f55a1f8729532b
weiwen <wt854@uowmail.edu.au>
2020-02-04 23:05:28 +0800

    First commit

---
+++ ui/src/Page.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Glyphicon,
+  Grid, Col,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Contents from './Contents.jsx';
+import IssueAddNavItem from './IssueAddNavItem.jsx';
+import SignInNavItem from './SignInNavItem.jsx';
+import Search from './Search.jsx';
+
+function NavBar() {
+  return (
+    <Navbar fluid>
+      <Navbar.Header>
+        <Navbar.Brand>Issue Tracker</Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer exact to="/">
+          <NavItem>Home</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/issues">
+          <NavItem>Issue List</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/report">
+          <NavItem>Report</NavItem>
+        </LinkContainer>
+      </Nav>
+      <Col sm={5}>
+        <Navbar.Form>
+          <Search />
+        </Navbar.Form>
+      </Col>
+      <Nav pullRight>
+        <IssueAddNavItem />
+        <SignInNavItem />
+        <NavDropdown
+          id="user-dropdown"
+          title={<Glyphicon glyph="option-vertical" />}
+          noCaret
+        >
+          <LinkContainer to="/about">
+            <MenuItem>About</MenuItem>
+          </LinkContainer>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      <hr />
+      <p className="text-center">
+        Owner: Weiwen Tan at 
+        {' '}
+        <a href="https://github.com/wt-854/pro-mern-stack-2">
+          GitHub repository
+        </a>
+        <br />
+        Description: Followed the book "Pro MERN Stack 2nd Edition"
+        <br />
+        Issues: Incomplete project - Chpt 14 (Google OAuth) and Chpt 15 (Deployment)
+      </p>
+      <p className="text-center">
+        Credits: Full source code available at this
+        {' '}
+        <a href="https://github.com/vasansr/pro-mern-stack-2">
+          GitHub repository
+        </a>
+      </p>
+    </small>
+  );
+}
+
+export default function Page() {
+  return (
+    <div>
+      <NavBar />
+      <Grid fluid>
+        <Contents />
+      </Grid>
+      <Footer />
+    </div>
+  );
+}
---
+++ ui/src/SignInNavItem.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import {
+  NavItem, Modal, Button, NavDropdown, MenuItem,
+} from 'react-bootstrap';
+
+import withToast from './withToast.jsx';
+
+class SigninNavItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showing: false,
+      disabled: true,
+      user: { signedIn: false, givenName: '' },
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.signOut = this.signOut.bind(this);
+    this.signIn = this.signIn.bind(this);
+  }
+
+  componentDidMount() {
+    const clientId = "514602094858-o59fflbbeb72lvoodnfu41obghef3ksp.apps.googleusercontent.com"
+    if (!clientId) return;
+    window.gapi.load('auth2', () => {
+      if (!window.gapi.auth2.getAuthInstance()) {
+        window.gapi.auth2.init({ client_id: clientId }).then(() => {
+          this.setState({ disabled: false });
+        });
+      }
+    });
+  }
+
+  async signIn() {
+    this.hideModal();
+    const { showError } = this.props;
+    let googleToken;
+    try {
+      const auth2 = window.gapi.auth2.getAuthInstance();
+      const googleUser = await auth2.signIn();
+      googleToken = googleUser.getAuthResponse().id_token;
+    } catch (error) {
+      showError(`Error authenticating with Google: ${error.error}`);
+    }
+
+    try {
+      const apiEndpoint = "http://localhost:8000/auth";
+      const response = await fetch(`${apiEndpoint}/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ google_token: googleToken }),
+      });
+      const body = await response.text();
+      const result = JSON.parse(body);
+      const { signedIn, givenName } = result;
+
+      this.setState({ user: { signedIn, givenName } });
+    } catch (error) {
+      showError(`Error signing into the app: ${error}`);
+    }
+  }
+
+  signOut() {
+    this.setState({ user: { signedIn: false, givenName: '' } });
+  }
+
+  showModal() {
+    const clientId = "514602094858-o59fflbbeb72lvoodnfu41obghef3ksp.apps.googleusercontent.com";
+    const { showError } = this.props;
+    if (!clientId) {
+      showError('Missing environment variable GOOGLE_CLIENT_ID');
+      return;
+    }
+    this.setState({ showing: true });
+  }
+
+  hideModal() {
+    this.setState({ showing: false });
+  }
+
+  render() {
+    const { user } = this.state;
+    if (user.signedIn) {
+      return (
+        <NavDropdown title={user.givenName} id="user">
+          <MenuItem onClick={this.signOut}>Sign out</MenuItem>
+        </NavDropdown>
+      );
+    }
+
+    const { showing, disabled } = this.state;
+    return (
+      <>
+        <NavItem onClick={this.showModal}>
+          Sign in
+        </NavItem>
+        <Modal keyboard show={showing} onHide={this.hideModal} bsSize="sm">
+          <Modal.Header closeButton>
+            <Modal.Title>Sign in</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Button
+              block
+              disabled={disabled}
+              bsStyle="primary"
+              onClick={this.signIn}
+            >
+              <img src="https://goo.gl/4yjp6B" alt="Sign In" />
+            </Button>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="link" onClick={this.hideModal}>Cancel</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default withToast(SigninNavItem);
